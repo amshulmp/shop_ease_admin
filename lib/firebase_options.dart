7376aea2f6565ea9c8ce6c04e1c4d1e4725e5523
amshulmp <161571699+amshulmp@users.noreply.github.com>
2024-04-09 11:53:29 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -53,8 +53,8 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAgqA08SROsAJPDZwmt0UygCFrCNypOGJ4',
-    appId: '1:371576593800:android:8c16b7e2a237d8fd2e6e3d',
+    apiKey: {your-api-key},
+    appId: {your-api-id}',
     messagingSenderId: '371576593800',
     projectId: 'ecommercebackend-a7c6d',
     storageBucket: 'ecommercebackend-a7c6d.appspot.com',
